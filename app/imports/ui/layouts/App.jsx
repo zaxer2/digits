f3ade80e063818bd1d7fd04bd98f0fca993c7633
zaxer2 <william.johns3@gmail.com>
2018-10-26 19:53:33 -1000

    Completed in 23 min

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -14,6 +14,7 @@ import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
+
 import Signout from '../pages/Signout';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
