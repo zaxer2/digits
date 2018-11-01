b00ff6bf2395709e2383a7cac1c25f81bd98895d
zaxer2 <william.johns3@gmail.com>
2018-11-01 12:38:20 -1000

    donezo

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,39 +1,63 @@
 import React from 'react';
-import { Image, Card, Feed } from 'semantic-ui-react';
+import { Image, Card, Feed, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import Note from './Note.jsx';
 import AddNote from './AddNote.jsx';
+import { Contacts } from '/imports/api/contact/contact';
+import { Bert } from 'meteor/themeteorchef:bert';
 
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Contact extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.deleteCallback = this.deleteCallback.bind(this);
+    this.formRef = null;
+  }
+
+  handleClick = () => Contacts.remove(this.props.contact._id, this.deleteCallback);
+
+
   render() {
-    return (
-            <Card>
-              <Card.Content>
-                <Image floated='right' size='mini' src={this.props.contact.image} />
-                <Card.Header>
-                  {this.props.contact.firstName} {this.props.contact.lastName}
-                  </Card.Header>
-                <Card.Meta>{this.props.contact.address}</Card.Meta>
-                <Card.Description>
-                  {this.props.contact.description}
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
-              </Card.Content>
-              <Card.Content extra>
-                <Feed>
-                  {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
-                </Feed>
-              </Card.Content>
-              <Card.Content extra>
-                <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
-              </Card.Content>
-            </Card>
-    );
+      return (
+          <Card>
+            <Card.Content>
+              <Image floated='right' size='mini' src={this.props.contact.image}/>
+              <Card.Header>
+                {this.props.contact.firstName} {this.props.contact.lastName}
+              </Card.Header>
+              <Card.Meta>{this.props.contact.address}</Card.Meta>
+              <Card.Description>
+                {this.props.contact.description}
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
+            </Card.Content>
+            <Card.Content extra>
+              <Feed>
+                {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
+              </Feed>
+            </Card.Content>
+            <Card.Content extra>
+              <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
+            </Card.Content>
+            <Card.Content extra>
+              <Button onClick={this.handleClick}>delete</Button>
+            </Card.Content>
+
+          </Card>
+      );
+  }
+
+  deleteCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Delete failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Delete succeeded' });
+    }
   }
 }
 
